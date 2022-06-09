78acfcf454e8e6fc9f32907f4fa8aea195ac11c9
Jesús Emilio Rodríguez <92540730+EmilioRXD@users.noreply.github.com>
2022-06-09 16:10:49 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const open = document.getElementById('open');
 const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
-
+const close = document.querySelector(".close");
 open.addEventListener('click', () => {
   modal_container.classList.add('show');  
 });
